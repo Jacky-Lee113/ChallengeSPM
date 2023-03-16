171fe232c23c5ac0873c393a2f95e206139df82f
Rui Felipe David <ruifelipe@gmail.com>
2023-03-16 14:54:49 -0400

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
+require 'open-uri'
+
 class HomeController < ApplicationController
   def index
+  end
+
+  def search_by_name
+    @name = params[:name]
+    @cocktails = URI.open('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + @name)
+
+    if @cocktails
+      @cocktails = JSON.parse(@cocktails.read)
+    end
+
+  #  render json: @cocktails['drinks']
+  end
+
+  def random
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+
+  root "home#index"
+  get '/search-by-name', to: "home#search_by_name"
+  get '/random', to: "home#random"
 end
